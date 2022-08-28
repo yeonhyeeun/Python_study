ad4f76d35802b4d9e647f3a7ef3a5510a1976c81
yeonhyeeun <0622yeon@naver.com>
2022-08-28 22:00:25 +0900

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,9 @@
+#demo.py 
+result = "hello world"
+print(result)
+
+my_name = "hyerong"
+my_age = "20"
+my_color_eyes = "brown"
+
+print(f"Hello I'm {my_name}, I have {my_age} years in the hyerong world, {my_color_eyes} is my eye color") 
