59a480216e5df311a1ae1c84ba9315fcdf1712cf
yeonhyeeun <0622yeon@naver.com>
2022-09-02 21:37:36 +0900

    TIL

--- day0902.py
+++ day0902.py
@@ -1,6 +1,15 @@
 '''
 파이썬에는 3가지 데이터구조가 있다. 
 1: list [] , week = [m,t,w,t,f,s,s]
-2: tuple
-3: dictionary
+내장된 메소드들을 활용하여 데이터 수정이 용이하다. index 0부터 시작 
+
+2: tuple (), t1 = (1)
+immutable, 불변성. 만든 이후 데이터를 바꿀수없다. 
+메소드도 안 뜸. index 0부터 시작 
+
+3: dictionary {}, dic = {key:value}
+key와 value를 한 쌍으로 갖는 자료형. mutable-데이터 수정 가능. 
+내장된 메소드 사용 가능 ex. pop, get 등 
+
+복습 포인트 : mutable 가능성 유무, tuple의 인덱스 세는 방법 -를 붙인다. 
 '''
