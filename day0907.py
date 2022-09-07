358601eeb92334329c7e54a0a9cafb166fee2432
yeonhyeeun <0622yeon@naver.com>
2022-09-08 04:46:08 +0900

    create day0908.py

--- day0907.py
+++ day0907.py
@@ -23,3 +23,26 @@ for website in websites:
 #request practice
 #import requests 
 
+from requests import get
+#import requests
+#standard lib에 없음. 인터넷을 통해 다운 받음
+
+websites = ("google.com", "airbnb.com", "https://twitter.com", "facebook.com",
+            "https://tiktok.com")
+
+results = {}
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
+    response = get(website)
+    #get함수는 웹사이트의 응답 그대로를 받아온다.
+    #print(response.status_code)
+    #http 상세코드 검색해보면 프로토콜마다의 의미를 알수있음
+    if response.status_code == 200:
+        results[website] = "ok"
+    else:
+        results[website] = "failed"
+
+print(results)
+
